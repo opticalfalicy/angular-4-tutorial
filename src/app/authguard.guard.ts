3b6e59d44d93f08f77ca7fb2ca7be5463eb9cdac
Unknown <opticalfalicy@gmail.com>
2019-01-22 17:38:46 -0600

    Lesson 20

--- src/app/authguard.guard.ts
+++ src/app/authguard.guard.ts
@@ -7,17 +7,20 @@ import {
 import { Observable } from "rxjs";
 import { UserService } from "./user-service.service";
 import { TouchSequence } from "selenium-webdriver";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthguardGuard implements CanActivate {
-  constructor(private user: UserService) {}
+  constructor(private user: UserService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
+    this.router.navigate(["/"]);
+    alert("Invalid Login. You are not authenticated!");
     return this.user.getUserLoggedIn();
   }
 }
