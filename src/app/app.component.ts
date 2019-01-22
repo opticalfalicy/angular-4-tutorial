bb38dc655351cdd663a5d4ec0a9b1c38ba31eca0
Unknown <opticalfalicy@gmail.com>
2019-01-22 16:13:57 -0600

    Up to lesson 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,36 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = "my-app";
-  visible = false;
-  counter = 0;
-  toggleDiv() {
-    // this.visible = !this.visible;
-    this.counter++;
-  }
+  username = "swedishgoodbye";
+
+  // changeUsername(event) {
+  //   this.username = event.target.value;
+  // }
+
+  // title = "my-app";
+  // visible = false;
+  // counter = 0;
+  // result = 2;
+  // myCustomString = "";
+
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.result = 50;
+  //   }, 2000);
+  // }
+
+  // myFunction(event) {
+  //   this.myCustomString += event.target.value + "\n";
+  // }
+
+  // toggleDiv() {
+  //   // this.visible = !this.visible;
+  //   this.counter++;
+  // }
+  // myFavLang = [
+  //   { name: "html", type: "frontend" },
+  //   { name: "css", type: "frontend" },
+  //   { name: "js", type: "frontend" },
+  //   { name: "ruby", type: "backend" }
+  // ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { BlueColoredDirective } from "./blue-colored.directive";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BlueColoredDirective } from './blue-colored.directive';
+const appRoutes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BlueColoredDirective
+    BlueColoredDirective,
+    DashboardComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
